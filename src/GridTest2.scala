ca9673451787a2b93eb81775d94ad5ffcfa3bc41
Biebzoom008 <theodelalayx@gmail.com>
2025-01-08 17:17:42 +0100

    16x16x32px grid done! Small area at the top 32*3+1 pixels

--- src/GridTest2.scala
+++ src/GridTest2.scala
@@ -6,28 +6,44 @@ import java.awt.Color
 object GridTest2 extends App {
 
   var largeur: Int = 512
-  var hauteur: Int = 600
+  var hauteur: Int = 608
   val display: FunGraphics = FunGraphics(largeur, hauteur)
 
-  var cellule: Int = 0
+  var cellulex: Int = 1
+  var celluley : Int = 1
 
-  display.setColor(new Color(26, 102, 46))
-  for (x: Int <- 0 until largeur) {
-    for (y: Int <- 0 until hauteur) {
-      display.setPixel(x, y)
-    }
-  }
+
+
+  val green : Color =  new Color(0,255,0)
+  val light_green : Color = new Color(0,160,0)
+  val blue : Color = new Color(0,200,255)
 
   for (x <- 0 until (largeur)) {
     for (y <- 0 until hauteur) {
-      if (cellule % 2 == 0) {
-        display.setPixel(x, y, new Color(0, 255, 0))
-      } else {
-        display.setPixel(x, y, new Color(0, 160, 0))
-      }
-      if (x % 32 == 0) {
-        cellule += 1
+
+      if(cellulex % 2 == 0 && celluley % 2 == 0){
+        display.setPixel(x,y,light_green)
+      } else if(cellulex%2==0 && celluley%2==1){
+        display.setPixel(x,y,green)
+      } else if (cellulex%2==1 && celluley%2==0){
+        display.setPixel(x,y,green)
+      } else {display.setPixel(x,y,light_green)}
+
+
+
+      if(y%32 ==0 && y>0){
+        celluley +=1
       }
     }
+    if (x % 32 == 0 && x>0) {
+      cellulex += 1
+    }
   }
+
+  for(x <- 0 until largeur){
+    for (y<- 0 until 32*3+1){
+      display.setPixel(x,y,blue)
+    }
+  }
+
 }
