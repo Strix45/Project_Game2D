afcceb7893104734bf70ac88562d5c0913984e54
Biebzoom008 <theodelalayx@gmail.com>
2025-01-15 10:02:26 +0100

    Berries spawn when eaten

--- src/Grid.scala
+++ src/Grid.scala
@@ -8,10 +8,11 @@ class Grid (){
   class Snake () {
     val position: Array[Int] = Array(7,10)
     var up : Boolean = false
-    var down : Boolean =  true
-    var right : Boolean = false
+    var down : Boolean =  false
+    var right : Boolean = true
     var left : Boolean = false
     var size : Int = 1
+    var score : Int = 0
   }
 
   class Food {
@@ -20,21 +21,31 @@ class Grid (){
     def eat() : Unit = {
       if (foodGrid(snake.position(0)-1)(snake.position(1)-4) == 1){
         snake.size +=1
+        snake.score +=1
+        foodGrid(snake.position(0)-1)(snake.position(1)-4) = 0
+      } else if (foodGrid(snake.position(0)-1)(snake.position(1)-4) == 2){
+        snake.size += 2
+        snake.score += 5
         foodGrid(snake.position(0)-1)(snake.position(1)-4) = 0
       }
     }
 
     var foodTimer : Int = 0
 
+
     def createFood() : Unit = {
       var randomX : Int = (Math.random()*15).toInt
       var randomY : Int = (Math.random()*15).toInt
       foodTimer += 1
-      if (foodTimer > 2 && gridElement(randomX)(randomY) == 0 && foodGrid(randomX)(randomY) == 0){
+      if (((foodGrid(snake.position(0)-1)(snake.position(1)-4) == 1) || (foodGrid(snake.position(0)-1)(snake.position(1)-4) == 2)) && gridElement(randomX)(randomY) == 0 && foodGrid(randomX)(randomY) == 0){
         foodTimer = 0
-
         display.drawTransformedPicture(cornerSize * 30 + randomX*cellSize + 15, headerSize * 30 + randomY*cellSize + 45, 0, 0.05, "/res/strawberry.png")
         foodGrid(randomX)(randomY) = 1
+      } else if (foodGrid(randomX)(randomY)==1){
+        drawEmpty(randomX,randomY)
+        display.drawTransformedPicture(cornerSize * 30 + randomX*cellSize + 15, headerSize * 30 + randomY*cellSize + 45, 0, 0.175, "/res/Goldberry_ingame.png")
+        foodGrid(randomX)(randomY) = 2
+
       }
     }
 
@@ -43,7 +54,6 @@ class Grid (){
   //Size of the display
   val width : Int = 512
   val height : Int = 600
-  val hi : Int = 2
   val cellSize : Int = 30
 
   //Size of header and space on the sides. Size in number of cells it represents
@@ -189,20 +199,35 @@ class Grid (){
         if (gridElement(x)(y)>0) {      //>0 so that no cell value ever goes under 0
           gridElement(x)(y) -= 1
         }
-        if (gridElement(x)(y) == 0 && ((x + y) % 2 == 0 && food.foodGrid(x)(y)==0) ){     //alternates the color of the cells and draws them where the cell's value is 0
-          drawCell(x + cornerSize, y + headerSize + 1, lightGreen)
-        } else if (gridElement(x)(y) == 0 && !((x + y) % 2 == 0) && food.foodGrid(x)(y)==0){
-          drawCell(x + cornerSize, y + headerSize + 1, green)
-        }
+        drawEmpty(x,y)
       }
     }
   }
 
+  def drawEmpty (x : Int, y : Int) : Unit = {
+    if (gridElement(x)(y) == 0 && ((x + y) % 2 == 0 && food.foodGrid(x)(y)==0) ){     //alternates the color of the cells and draws them where the cell's value is 0
+      drawCell(x + cornerSize, y + headerSize + 1, lightGreen)
+    } else if (gridElement(x)(y) == 0 && !((x + y) % 2 == 0) && food.foodGrid(x)(y)==0){
+      drawCell(x + cornerSize, y + headerSize + 1, green)
+    }
+  }
+
+
+  def scoreDisplay () : Unit = {
+    display.drawString(width/6,height/8, s"This is your score : ${snake.score}","Arial",Font.PLAIN,20, blue)
+  }
 
   def deathScreen() : Unit = {
     display.clear()
     display.drawString(width/4,height/2, "YOU DIEDDDDD", blue, 20)
+    scoreDisplay()
   }
 
+
+  //TODO Start screen (mouse control)
+  //TODO Death screen revamped and restart option
+  //TODO Settings
+  //TODO Music (?)
+  //TODO Sound effects
 
 }
--- src/Main.scala
+++ src/Main.scala
@@ -17,29 +17,33 @@ object Main {
 
     grid.display.drawForeground()
 
-
-    grid.display.syncGameLogic(60)
-
+    var randomX : Int = (Math.random()*15).toInt
+    var randomY : Int = ((Math.random()*15).toInt)
+    grid.display.syncGameLogic(30)
+    grid.food.foodGrid(randomX)(randomY)= 1
+    grid.display.drawTransformedPicture(grid.cornerSize * 30 + randomX*grid.cellSize + 15, grid.headerSize * 30 + randomY*grid.cellSize + 45, 0, 0.05, "/res/strawberry.png")
     try{while(!grid.tailDeath){
       grid.move()
       grid.drawGame()
 
+      if((Math.random()+1).toInt ==1){
       grid.food.createFood()
+      } else {
+        grid.food.createFood()
+        grid.food.createFood()
+      }
+
       grid.food.eat()
-      println(s"${grid.snake.position(0)} and ${grid.snake.position(1)}")
-      Thread.sleep(300)
-      } }catch {
+      grid.drawHeader(grid.headerGreen)
+      grid.scoreDisplay()
+      Thread.sleep(250)
+      }
+    } catch {
       case e : ArrayIndexOutOfBoundsException => grid.deathScreen()
     }
 
     grid.deathScreen()
     }
-
-
-
-
-
-
 
 }
 
